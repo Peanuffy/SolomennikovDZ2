421714a80f4ac2e2522ee56037677d2c5b0c08ee
Peanuffy <vanuasoloma@gmail.com>
2024-11-24 13:10:02 +0500

    Add files via upload

---
+++ SolomennikovDZ2_2/SolomennikovDZ2_2/SolomennikovDZ2_2.py
@@ -0,0 +1,22 @@
+import numpy as np
+import os
+
+matrix = np.load('second_task.npy')
+
+threshold = 525 
+
+indices = np.where(matrix > threshold)
+
+values = matrix[indices]
+
+index_array = list(zip(indices[0], indices[1]))
+
+np.savez('second_task.npz', indices=index_array, values=values)
+np.savez_compressed('compressed_second_task.npz', indices=index_array, values=values)
+
+original_size = os.path.getsize('second_task.npz')
+compressed_size = os.path.getsize('compressed_second_task.npz')
+
+print(f'Original size: {original_size / 1024:.2f} KB')
+print(f'Compressed size: {compressed_size / 1024:.2f} KB')
+
